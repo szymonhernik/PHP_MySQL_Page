4585a5b9caaa00feeb23ee6ffdc0c663c9ce6431
Simon Hernik <szymonhernik123@gmail.com>
2017-04-08 11:51:33 +0200

    PHP MySQL project with Lynda

---
+++ public/logout.php
@@ -0,0 +1,24 @@
+<?php require_once("../includes/session.php"); ?>
+<?php require_once("../includes/functions.php"); ?>
+
+<?php
+  //v1: simple logout
+  //session start()
+
+  $_SESSION["admin_id"] = null;
+  $_SESSION["username"] = null;
+  redirect_to("login.php");
+
+
+?>
+<?php
+  //v2: destroy session
+
+//  session_start();
+//  $_SESSION = array();
+//  if(isset($_COOKIE[session_name()])) {
+//    setcookie(session_name(), '', time()-42000, '/');
+//  }
+//  session_destroy();
+//redirect_to("login.php");
+?>
